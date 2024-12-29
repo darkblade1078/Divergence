99d840b292049962c4978438d56e10d3df9fdffc
darkblade <39814430+darkblade1078@users.noreply.github.com>
2024-12-29 15:14:15 -0600

    Delete build directory

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("./utils/client");
-require("dotenv/config");
-const client = new client_1.WardenClient();
-client.login(process.env.DISCORD_BOT_TOKEN);
